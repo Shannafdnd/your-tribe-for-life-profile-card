b810088d0232afd6f1c34ed339a36fe8946ee441
Shannafdnd <144009693+Shannafdnd@users.noreply.github.com>
2024-09-03 17:15:40 +0200

    tutorial done, added svelte/directus

---
+++ src/routes/blog/[slug]/+page.js
@@ -0,0 +1,16 @@
+/** @type {import('./$types').PageLoad} */
+import { error } from '@sveltejs/kit';
+import getDirectusInstance from '$lib/directus';
+import { readItem } from '@directus/sdk';
+export async function load({ fetch, params }) {
+	const directus = getDirectusInstance(fetch);
+	try {
+		return {
+			post: await directus.request(readItem('posts', params.slug, {
+				fields: ['*', { '*': ['*'] }],
+			})),
+		};
+	} catch (err) {
+		error(404, 'Post not found');
+	}
+}
