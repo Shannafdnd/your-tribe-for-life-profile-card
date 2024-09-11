9236b656ca1d27a11c23502f908b0a6d7e3512e0
Shannafdnd <144009693+Shannafdnd@users.noreply.github.com>
2024-09-11 14:20:38 +0200

    Removed tutorial pages

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,18 @@
 /** @type {import('./$types').PageLoad} */
 import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
+
+const primaryColors = ["ff0000", "00ff00"];
+const secondaryColors = ["00ffff", "ff00ff"];
+
+function pickRandom(array) {
+    return array[Math.floor(Math.random()*array.length)];
+}
+
+function updateColors() {
+    
+}
+
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
 	return {
--- src/routes/blog/[slug]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { PUBLIC_APIURL } from '$env/static/public';
-	/** @type {import('./$types').PageData} */
-	export let data;
-</script>
-<img src="{PUBLIC_APIURL}/assets/{data.post.image.filename_disk}?width=600" alt="{data.post.image.description}" />
-<h1>{data.post.title}</h1>
-<div>{@html data.post.content}</div>
