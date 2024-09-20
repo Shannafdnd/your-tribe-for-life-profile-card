6c4d16973729dcb3442f32701a77c66e085ac5db
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-09-20 12:26:59 +0200

    inhoud af (css nog niet af)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 	export let data;
 	const person = data.people[0];
 
-	const primaryColors = ["#ff0000", "#00ff00"];
-	const secondaryColors = ["#00ffff", "#ff00ff"];
+	const primaryColors = ["ACB4FF", "ADFFF5", "C1FFB1", "FFB7E2", "FFDBB0", "E9ACFF" ];
+	const secondaryColors = ["3B4ACE", "29C2A6", "22AA27", "AB2977", "A5422C", "742390" ];
 
 	function pickRandom(array) {
 		return array[Math.floor(Math.random()*array.length)];
@@ -22,10 +22,31 @@
 
 <style> @import "/styles/main.css"; </style>
 
-<main style="--primary-color: {primaryColor};--secondary-color: {secondaryColor}">
-	<h1>{person.name} {person.prefix} {person.surname}</h1>
-	<img src={person.avatar} alt={person.name}>
-	<a href="https://github.com/{person.github_handle}" target="_blank">Github</a>
-	<p>{person.bio}</p>
-	<button on:click={updateColors}>New color</button>
+<header>
+	
+</header>
+<main style="--primary-color: #{primaryColor};--secondary-color: #{secondaryColor}">
+	<article>
+		<div class="name-and-photo">
+			<img id="avatar" src={person.avatar} alt={person.name}>
+			<button on:click={updateColors}>
+				<img src="/palette.svg" alt="palette">
+			</button>
+			<div id="right-rectangle">
+				<div id="top-float-rectangle"></div>
+				<p>{person.bio}</p>
+				<div class="socials">
+					<a class="logo" href="https://github.com/{person.github_handle}" target="_blank"><img src="/GitHub_Invertocat_Logo.svg" alt="Github"></a>
+					<a class="logo" href="{person.website}" target="_blank"><img src="/free-instagram-86-433323.webp" alt="instagram"></a>
+					<a class="logo" href="mailto:{person.email}" target="_blank"><img src="/free-gmail-32-761667.png" alt="gmail"></a>
+				</div>
+				<div id="right-float-rectangle"></div>
+			</div>
+
+			<h1>{person.name} {person.prefix} {person.surname}</h1>
+		</div>
+	</article>
 </main>
+<footer>
+
+</footer>
